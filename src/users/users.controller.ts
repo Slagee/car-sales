8760dac7040fb3c67b76a86a5b011647fabb51c4
Slagee <lukas.lazar.42@gmail.com>
2023-07-20 23:54:25 +0200

    Basic type checking

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,6 +6,7 @@ import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
 
     constructor(private usersService: UsersService) {}
@@ -15,7 +16,6 @@ export class UsersController {
         this.usersService.create(body.email, body.password);
     }
 
-    @Serialize(UserDto)
     @Get('/:id')
     async findUser(@Param('id') id: string) {
         const user = await this.usersService.findOne(parseInt(id));
